2198381aab3582935873bec5775f4660352e0f8f
Arman <ara@onyxtec.co>
2025-02-28 16:39:19 +0500

    delete addresses, devices and some minor fixes

--- resources/js/scripts/pages/shipping-addresses.js
+++ resources/js/scripts/pages/shipping-addresses.js
@@ -13,96 +13,61 @@ $(function () {
                     data: "created_at",
                     render: function (data, type, full, meta) {
                         let date = new Date(data);
-                        return date.toLocaleString('en-US', {
-                            weekday: 'long',
-                            year: 'numeric',
-                            month: 'long', 
-                            day: 'numeric',
-                            hour: '2-digit',
-                            minute: '2-digit',
-                            second: '2-digit',
-                            hour12: true
-                        });
+                        return (
+                            ("0" + (date.getMonth() + 1)).slice(-2) + "/" +
+                            ("0" + date.getDate()).slice(-2) + "/" +
+                            date.getFullYear()
+                        );
                     }
                 },
+                {
+                    data: null,
+                    orderable: false,
+                    render: function (data, type, full, meta) {
+                        return (
+                            '<div class="btn-group">' +
+                            '<a class="btn btn-sm dropdown-toggle hide-arrow" data-toggle="dropdown">' +
+                            feather.icons["more-vertical"].toSvg({ class: "font-small-4" }) +
+                            "</a>" +
+                            '<div class="dropdown-menu dropdown-menu-right">' +
+                            '<a href="#" class="dropdown-item delete-record" data-id="' + full["id"] + '">' +
+                            feather.icons["trash-2"].toSvg({ class: "font-small-4 mr-50" }) +
+                            "Delete</a></div>" +
+                            "</div>"
+                        );
+                    }
+                }
             ],
             order: [[2, "desc"]],
-            dom: '<"card-header border-bottom p-1"<"head-label"><"dt-action-buttons text-right"B>><"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+            dom: '<"card-header border-bottom p-1"<"head-label"><"dt-action-buttons text-right">><"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
             displayLength: 10,
             lengthMenu: [10, 25, 50, 100],
-            buttons: [
-                {
-                    extend: "collection",
-                    className: "btn btn-outline-secondary dropdown-toggle mr-2",
-                    text:
-                        feather.icons["share"].toSvg({ class: "font-small-4 mr-50" }) +
-                        "Export",
-                    buttons: [
-                        {
-                            extend: "print",
-                            text:
-                                feather.icons["printer"].toSvg({
-                                    class: "font-small-4 mr-50",
-                                }) + "Print",
-                            className: "dropdown-item",
-                            exportOptions: { columns: [1, 2] },
-                        },
-                        {
-                            extend: "csv",
-                            text:
-                                feather.icons["file-text"].toSvg({
-                                    class: "font-small-4 mr-50",
-                                }) + "CSV",
-                            className: "dropdown-item",
-                            exportOptions: { columns: [1, 2] },
-                        },
-                        {
-                            extend: "excel",
-                            text:
-                                feather.icons["file"].toSvg({ class: "font-small-4 mr-50" }) +
-                                "Excel",
-                            className: "dropdown-item",
-                            exportOptions: { columns: [1, 2] },
-                        },
-                        {
-                            extend: "pdf",
-                            text:
-                                feather.icons["clipboard"].toSvg({
-                                    class: "font-small-4 mr-50",
-                                }) + "PDF",
-                            className: "dropdown-item",
-                            exportOptions: { columns: [1, 2] },
-                        },
-                        {
-                            extend: "copy",
-                            text:
-                                feather.icons["copy"].toSvg({ class: "font-small-4 mr-50" }) +
-                                "Copy",
-                            className: "dropdown-item",
-                            exportOptions: { columns: [1, 2] },
-                        },
-                    ],
-                    init: function (api, node, config) {
-                        $(node).removeClass("btn-secondary");
-                        $(node).parent().removeClass("btn-group");
-                        setTimeout(function () {
-                            $(node)
-                                .closest(".dt-buttons")
-                                .removeClass("btn-group")
-                                .addClass("d-inline-flex");
-                        }, 50);
-                    },
-                },
-            ],
             language: {
-                paginate: {
-                    previous: "&nbsp;",
-                    next: "&nbsp;",
-                },
+                paginate: { previous: "&nbsp;", next: "&nbsp;" },
                 sLengthMenu: "Show _MENU_",
-                search: "Search",
-                searchPlaceholder: "Search Shipping Addresses...",
+                search: "Search Shipping Addresses...",
             },
+        });
+
+        // Delete Shipping Address
+        $(document).on("click", ".delete-record", function () {
+            var id = $(this).data("id");
+
+            if (confirm("Are you sure you want to delete this address?")) {
+                $.ajax({
+                    url: "/shipping-addresses/" + id,
+                    type: "DELETE",
+                    headers: {
+                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                    },
+                    success: function (response) {
+                        location.reload(true);
+                    },
+                    error: function (xhr, status, error) {
+                        location.reload(true);
+                    }
+                });
+            }
         });
     }
 });
